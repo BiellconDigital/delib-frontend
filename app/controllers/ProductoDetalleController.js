b93b26bb18f83ef2de8de6f1a3a6d233777d6f4e
Antonio Ramos <tonyprr@gmail.com>
2014-03-13 23:38:51 -0500

    carousel de imagenes de productos

--- app/controllers/ProductoDetalleController.js
+++ app/controllers/ProductoDetalleController.js
@@ -18,6 +18,8 @@ define(['app'], function (app) {
         $scope.productos = {};
          $scope.likeURL = $sce.trustAsResourceUrl('http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.delibouquet.pe%2Fcart%2Findex.html%23%2Fdetalle-producto%2F' + $stateParams.prodId + '&width&layout=standard&action=like&show_faces=false&share=false&height=80');
         
+        $('.carousel').carousel();
+        
         var load = function() {
 //            console.log('call load()...');
             $http.get($rootScope.appUrl + '/producto', {params: { operacion : 'getById', idprod: $stateParams.prodId }})
